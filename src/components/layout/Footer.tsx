c4b766a292bd62fcd6a517931a1dabdeb9da991d
Dev Jeffrey <mdog1525@gmail.com>
2024-11-23 22:46:18 -0500

    create Footer componet with Flowbite component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,35 @@
-
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bottom-0 w-full p-4 bg-white border-t border-accent shadow grid md:grid-cols-2 md:gap-x-4 gap-y-4 items-center justify-between md:p-6">
+      <span className="text-sm text-black sm:text-center">
+        © 2024{" "}
+        <a href="#" className="hover:underline">
+          CuidandoPaws™
+        </a>
+        . Todos los derechos reservados.
+      </span>
+      <ul className="flex flex-wrap justify-center items-center mt-3 text-sm font-medium text-black sm:mt-0">
+        <li>
+          <a href="#" className="hover:underline me-4 md:me-6">
+            Acerca de
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline me-4 md:me-6">
+            Politica de privacidad
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline me-4 md:me-6">
+            Licencia
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">
+            Contacto
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
